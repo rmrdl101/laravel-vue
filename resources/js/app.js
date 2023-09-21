5ed671ac15af5118041d572560b6011e510c8a90
Roland Lagos <rmrlagos@gmail.com>
2023-09-21 09:59:22 +0800

    updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,13 @@ import './bootstrap';
 import 'admin-lte/plugins/jquery/jquery.min.js';
 import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
 import 'admin-lte/dist/js/adminlte.min.js';
+
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const app = createApp({});
+
+const router = createRouter({
+    routes: '',
+    history: createWebHistory(),
+});
