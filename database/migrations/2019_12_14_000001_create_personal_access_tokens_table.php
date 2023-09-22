a00b45aef892ea123ac22b806db93d106595512d
Roland Lagos <rmrlagos@gmail.com>
2023-09-22 11:18:32 +0800

    linked and able to fetch data from user table

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,13 +13,16 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
+            
+            // Specify a custom index length for tokenable_type
+            $table->string('tokenable_type', 191)->index();
+            $table->unsignedBigInteger('tokenable_id')->index();
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ApplicationController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Route::get('/api/users', [UserController::class, 'index']);
+
 Route::get('{view}', ApplicationController::class)->where('view' , '(.*)');
